bdde4d296de2e6503df8f74f3e150bb0b124d1ff
supipis <70693604+supipis@users.noreply.github.com>
2021-06-07 17:27:16 +0200

    instructions added to the levels

--- script.js
+++ script.js
@@ -32,7 +32,19 @@ document.onkeydown = (event) => {
       shoot();
     }
   };
-  
+
+function hide() {
+  let x = document.getElementById('hide-div')
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+
 
 
 
